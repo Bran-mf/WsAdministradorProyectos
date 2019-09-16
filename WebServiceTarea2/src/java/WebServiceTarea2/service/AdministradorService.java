5f0257072c42d729341badc12a74dcf5dc66ba10
Brandon mata <brand9613@hotmail.com>
2019-09-16 00:24:30 -0600

    metodo funcional

--- WebServiceTarea2/src/java/WebServiceTarea2/service/AdministradorService.java
+++ WebServiceTarea2/src/java/WebServiceTarea2/service/AdministradorService.java
@@ -5,10 +5,25 @@
  */
 package WebServiceTarea2.service;
 
+import WebServiceTarea2.util.Respuesta;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  *
  * @author Jose Pablo Bermudez
  */
+@Stateless
+@LocalBean
 public class AdministradorService {
-    
+    @PersistenceContext(unitName = "WebServiceTarea2PU")
+    private EntityManager em;
+    public Respuesta validarUsuario(String usuario, String clave){
+        return null;
+//        try{
+//            Query query = em.createNamedQuery("")
+//        }
+  }
 }
---
+++ WebServiceTarea2/src/java/WebServiceTarea2/service/Service.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WebServiceTarea2.service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Bran
+ */
+public class Service {
+    @PersistenceContext(unitName = "WebServiceTarea2PU")
+    private EntityManager em ;
+    public EntityManager getem(){
+        return em;
+    }
+}
