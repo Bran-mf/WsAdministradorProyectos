e8cb769abc200236b6c5a2d6e73cd59821c789df
Brandon mata <brand9613@hotmail.com>
2019-09-17 15:35:20 -0600

    sd

--- WebServiceTarea2/src/java/WebServiceTarea2/service/AdministradorService.java
+++ WebServiceTarea2/src/java/WebServiceTarea2/service/AdministradorService.java
@@ -5,11 +5,16 @@
  */
 package WebServiceTarea2.service;
 
+import WebServiceTarea2.model.Administrador;
+import WebServiceTarea2.model.AdministradorDto;
+import WebServiceTarea2.util.CodigoRespuesta;
 import WebServiceTarea2.util.Respuesta;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -21,9 +26,16 @@ public class AdministradorService {
     @PersistenceContext(unitName = "WebServiceTarea2PU")
     private EntityManager em;
     public Respuesta validarUsuario(String usuario, String clave){
-        return null;
        try{
-          Query query = em.createNamedQuery("Administrador.")
+          Query query = em.createNamedQuery("Aminsitrador.findbyUsuClave",Administrador.class);
+          query.setParameter("usuario", usuario);
+          query.setParameter("clave", clave);
+          return  new Respuesta(true, CodigoRespuesta.CORRECTO,"","","Administrador",new AdministradorDto((Administrador) query.getSingleResult()));
+       }catch(NoResultException ex){
+         return  new Respuesta(false,CodigoRespuesta.ERROR_NOENCONTRADO,"No existe un usuario con las credenciales ingresadas","alidar usuario No result exception");  
+       } catch(Exception es){
+           return new Respuesta(false,CodigoRespuesta.ERROR_INTERNO,"Ocurrio un error con la consulta","Error desconocido Exception");
        }
   }
+    
 }
