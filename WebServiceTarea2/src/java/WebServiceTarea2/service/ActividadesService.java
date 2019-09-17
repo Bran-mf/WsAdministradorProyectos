902ac01a5389e59da18408b6841527d04830daac
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2019-09-16 22:08:51 -0600

    a

--- WebServiceTarea2/src/java/WebServiceTarea2/service/ActividadesService.java
+++ WebServiceTarea2/src/java/WebServiceTarea2/service/ActividadesService.java
@@ -12,7 +12,7 @@ import javax.persistence.PersistenceContext;
  *
  * @author Bran
  */
-public class Service {
+public class ActividadesService {
     @PersistenceContext(unitName = "WebServiceTarea2PU")
     private EntityManager em ;
     public EntityManager getem(){
