24b3f4d631fb1f29014e0ec4a1b90a93678cbff4
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2019-09-22 18:00:46 -0600

    Update ProyectoDto.java

--- WebServiceTarea2/src/java/WebServiceTarea2/model/ProyectoDto.java
+++ WebServiceTarea2/src/java/WebServiceTarea2/model/ProyectoDto.java
@@ -22,199 +22,153 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 @XmlRootElement(name = "ProyectoDto")
 @XmlAccessorType(XmlAccessType.PUBLIC_MEMBER)
 public class ProyectoDto {
-    private Long id;
-    private String nombre;
-    private String patrocinador;
-    private String liderUsuario;
-    private String liderTectnico;
-     @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    private LocalDate inicioEsperado;
-    @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    private LocalDate finalEsperado;
-    private String estado;
-    private String correoLiderUsuario;
-    private String correoLiderTecnico;
-    private String correoPatrocinador;
-    private Long version;
-    @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    private LocalDate inicioReal;
-    @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    private LocalDate finalReal;
-    
-    
-    //constructores
+    private Long proId;
+    private String proNombre;
+    private String proPatrocinador;
+    private String proLiderusuario;
+    private String proLidertecnico;
+    private String proCorreopatrocinador;
+    private String proCorreousuario;
+    private String proCorreotecnico;
+    private String proFechainireal;
+    private String proFechafinreal;
+    private String proFechainicio;
+    private String proFechafinal;
+    private String proEstado;
+    private Long proVersion;
 
     public ProyectoDto() {
     }
 
-    public ProyectoDto(Long id, String nombre, String patrocinador, String liderUsuario, String liderTectnico, LocalDate inicioEsperado, LocalDate finalEsperado, String estado, String correoLiderUsuario, String correoLiderTecnico, String correoPatrocinador, Long version, LocalDate inicioReal, LocalDate finalReal) {
-        this.id = id;
-        this.nombre = nombre;
-        this.patrocinador = patrocinador;
-        this.liderUsuario = liderUsuario;
-        this.liderTectnico = liderTectnico;
-        this.inicioEsperado = inicioEsperado;
-        this.finalEsperado = finalEsperado;
-        this.estado = estado;
-        this.correoLiderUsuario = correoLiderUsuario;
-        this.correoLiderTecnico = correoLiderTecnico;
-        this.correoPatrocinador = correoPatrocinador;
-        this.version = version;
-        this.inicioReal = inicioReal;
-        this.finalReal = finalReal;
-    }
-
     
-    public ProyectoDto(Proyecto proyecto){
-        this.id = proyecto.getPryId();
-        this.nombre = proyecto.getPryNombre();
-        this.patrocinador = proyecto.getPryPatrocinador();
-        this.liderUsuario = proyecto.getPryLiderusuario();
-        this.liderTectnico = proyecto.getPyrLidertecnico();
-        this.finalEsperado = proyecto.getPryFinalesperado().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        this.finalReal = proyecto.getPryFinalreal().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        this.inicioReal = proyecto.getPryInicioreal().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        this.inicioEsperado = proyecto.getPyrInicioesperado().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        this.correoLiderTecnico= proyecto.getPryCorreotenico();
-        this.correoLiderUsuario = proyecto.getPryCorreousuario();
-        this.correoPatrocinador = proyecto.getPryCorreopatrocinador();
-        this.version = proyecto.getPryVersion();
-        this.estado= proyecto.getPryEstado();           
-    }
-//    public  void Actuallizar(ProyectoDto proyecto){
-//
-//        this.nombre = proyecto.getNombre();
-//        this.patrocinador = proyecto.getPatrocinador();
-//        this.liderUsuario = proyecto.getLiderUsuario();
-//        this.liderTectnico = proyecto.getLiderTectnico();
-//        this.finalEsperado = proyecto.getFinalEsperado().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-//        this.finalReal = Date.from(proyecto.getFinalReal().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
-//        this.inicioReal = proyecto.getInicioReal().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-//        this.inicioEsperado = proyecto.getInicioEsperado()
-//        this.correoLiderTecnico= proyecto.getCorreoLiderTecnico();
-//        this.correoLiderUsuario = proyecto.getCorreoLiderUsuario();
-//        this.correoPatrocinador = proyecto.getCorreoPatrocinador();
-//        this.version = proyecto.getVersion();
-//        this.estado= proyecto.getEstado();           
-//    }
-    /*public Proyecto getModel(){
-        return new Proyecto(id, nombre, patrocinador, liderUsuario, liderTectnico, inicioEsperado, finalEsperado, estado, correoLiderUsuario, correoLiderTecnico, correoPatrocinador, version);
-    }*/
-    
-    
-    //metodos
-    //get and set
-    public Long getId() {
-        return id;
+    public ProyectoDto(Proyecto proyecto) {
+        this.proCorreopatrocinador = proyecto.getProCorreopatrocinador();
+        this.proCorreotecnico = proyecto.getProCorreotenico();
+        this.proCorreousuario = proyecto.getProCorreousuario();
+        this.proEstado = proyecto.getProEstado();
+        this.proFechafinreal = proyecto.getProFinalreal().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().toString();
+        this.proFechainireal = proyecto.getProInicioreal().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().toString();
+        this.proFechainicio = proyecto.getPyrInicioesperado().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().toString();
+        this.proFechafinal = proyecto.getProFinalesperado().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().toString();
+        this.proId = proyecto.getProId();
+        this.proLidertecnico = proyecto.getPyrLidertecnico();
+        this.proLiderusuario = proyecto.getProLiderusuario();
+        this.proNombre = proyecto.getProNombre();
+        this.proPatrocinador = proyecto.getProPatrocinador();
+        this.proVersion = proyecto.getProVersion();
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    public String getNombre() {
-        return nombre;
+    public Long getProId() {
+        return proId;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setProId(Long proId) {
+        this.proId = proId;
     }
 
-    public String getPatrocinador() {
-        return patrocinador;
+    public String getProNombre() {
+        return proNombre;
     }
 
-    public void setPatrocinador(String patrocinador) {
-        this.patrocinador = patrocinador;
+    public void setProNombre(String proNombre) {
+        this.proNombre = proNombre;
     }
 
-    public String getLiderUsuario() {
-        return liderUsuario;
+    public String getProPatrocinador() {
+        return proPatrocinador;
     }
 
-    public void setLiderUsuario(String liderUsuario) {
-        this.liderUsuario = liderUsuario;
+    public void setProPatrocinador(String proPatrocinador) {
+        this.proPatrocinador = proPatrocinador;
     }
 
-    public String getLiderTectnico() {
-        return liderTectnico;
+    public String getProLiderusuario() {
+        return proLiderusuario;
     }
 
-    public void setLiderTectnico(String liderTectnico) {
-        this.liderTectnico = liderTectnico;
-    }
-  @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    public LocalDate getInicioEsperado() {
-        return inicioEsperado;
-    }
-  @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    public void setInicioEsperado(LocalDate inicioEsperado) {
-        this.inicioEsperado = inicioEsperado;
-    }
-  @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    public LocalDate getFinalEsperado() {
-        return finalEsperado;
-    }
-  @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    public void setFinalEsperado(LocalDate finalEsperado) {
-        this.finalEsperado = finalEsperado;
+    public void setProLiderusuario(String proLiderusuario) {
+        this.proLiderusuario = proLiderusuario;
     }
 
-    public String getEstado() {
-        return estado;
+    public String getProLidertecnico() {
+        return proLidertecnico;
     }
 
-    public void setEstado(String estado) {
-        this.estado = estado;
+    public void setProLidertecnico(String proLidertecnico) {
+        this.proLidertecnico = proLidertecnico;
     }
 
-    public String getCorreoLiderUsuario() {
-        return correoLiderUsuario;
+    public String getProCorreopatrocinador() {
+        return proCorreopatrocinador;
     }
 
-    public void setCorreoLiderUsuario(String correoLiderUsuario) {
-        this.correoLiderUsuario = correoLiderUsuario;
+    public void setProCorreopatrocinador(String proCorreopatrocinador) {
+        this.proCorreopatrocinador = proCorreopatrocinador;
     }
 
-    public String getCorreoLiderTecnico() {
-        return correoLiderTecnico;
+    public String getProCorreousuario() {
+        return proCorreousuario;
     }
 
-    public void setCorreoLiderTecnico(String correoLiderTecnico) {
-        this.correoLiderTecnico = correoLiderTecnico;
+    public void setProCorreousuario(String proCorreousuario) {
+        this.proCorreousuario = proCorreousuario;
     }
 
-    public String getCorreoPatrocinador() {
-        return correoPatrocinador;
+    public String getProCorreotecnico() {
+        return proCorreotecnico;
     }
 
-    public void setCorreoPatrocinador(String correoPatrocinador) {
-        this.correoPatrocinador = correoPatrocinador;
+    public void setProCorreotecnico(String proCorreotecnico) {
+        this.proCorreotecnico = proCorreotecnico;
     }
 
-    public Long getVersion() {
-        return version;
+    public String getProFechainireal() {
+        return proFechainireal;
     }
 
-    public void setVersion(Long version) {
-        this.version = version;
+    public void setProFechainireal(String proFechainireal) {
+        this.proFechainireal = proFechainireal;
     }
-  @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    public LocalDate getInicioReal() {
-        return inicioReal;
+
+    public String getProFechafinreal() {
+        return proFechafinreal;
     }
-  @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    public void setInicioReal(LocalDate inicioReal) {
-        this.inicioReal = inicioReal;
+
+    public void setProFechafinreal(String proFechafinreal) {
+        this.proFechafinreal = proFechafinreal;
     }
-  @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    public LocalDate getFinalReal() {
-        return finalReal;
+
+    public String getProEstado() {
+        return proEstado;
     }
-  @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    public void setFinalReal(LocalDate finalReal) {
-        this.finalReal = finalReal;
+
+    public void setProEstado(String proEstado) {
+        this.proEstado = proEstado;
+    }
+
+    public String getProFechainicio() {
+        return proFechainicio;
+    }
+
+    public void setProFechainicio(String proFechainicio) {
+        this.proFechainicio = proFechainicio;
+    }
+
+    public String getProFechafinal() {
+        return proFechafinal;
+    }
+
+    public void setProFechafinal(String proFechafinal) {
+        this.proFechafinal = proFechafinal;
     }
     
+    public Long getProVersion() {
+        return proVersion;
+    }
+
+    public void setProVersion(Long proVersion) {
+        this.proVersion = proVersion;
+    }
+
     
 }
