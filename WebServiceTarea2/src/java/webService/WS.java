6d8acc8d91f332b1d1b7ba65501cfa9c5f5db055
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2019-09-27 14:51:25 -0600

    Update WS.java

--- WebServiceTarea2/src/java/webService/WS.java
+++ WebServiceTarea2/src/java/webService/WS.java
@@ -5,14 +5,19 @@
  */
 package webService;
 
+import WebServiceTarea2.model.ActividadesDto;
 import WebServiceTarea2.model.Administrador;
 import WebServiceTarea2.model.AdministradorDto;
 import WebServiceTarea2.model.ProyectoDto;
+import WebServiceTarea2.model.SeguimientoDto;
+import WebServiceTarea2.service.ActividadesService;
 import WebServiceTarea2.service.AdministradorService;
 import WebServiceTarea2.service.ProyectoService;
+import WebServiceTarea2.service.SeguimientoService;
 import WebServiceTarea2.util.CodigoRespuesta;
 import WebServiceTarea2.util.Respuesta;
 import static com.sun.corba.se.impl.util.Utility.printStackTrace;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -28,32 +33,27 @@ import javax.jws.WebParam;
 @WebService(serviceName = "WS")
 public class WS {
     @EJB
+    ActividadesService actividadService;
+    @EJB
     AdministradorService administradorService;
     @EJB
     ProyectoService proyectoService;
+    @EJB
+    SeguimientoService seguimientoService;
 
     @WebMethod(operationName = "getUsuario")
     public Respuesta getUsuario(@WebParam(name = "usuario") String usuario, @WebParam(name = "clave") String clave) {
         try {
+            // Consulto el service del Administrador y devuelvo la respuesta al cliente
             Respuesta respuesta = administradorService.validarAdministrador(usuario, clave);
             return respuesta;
         } catch (Exception ex) {
             Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
-            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Error al obtener el Usuario", "getUsuario:" + ex.getMessage());
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Error al obtener el Usuario", "getUsuario:"+ex.getMessage());
         }
     }
 
-    @WebMethod(operationName = "guardarAdministrador")
-    public Respuesta guardarAdministrador(@WebParam(name = "Administrador") AdministradorDto Administrador) {
-        try {
-            Respuesta respuesta = administradorService.guardarAdministrador(Administrador);
-            return respuesta;
-        } catch (Exception ex) {
-            Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
-            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Error guardando usuario", ex.getMessage());
-        }
-    }
-
+    
     @WebMethod(operationName = "eliminarAdministrador")
     public Respuesta EliminarAdministrador(@WebParam(name = "ID") Long ID) {
         try {
@@ -61,10 +61,9 @@ public class WS {
             return respuesta;
         } catch (Exception ex) {
             Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
-            return new Respuesta(false, CodigoRespuesta.ERROR_CLIENTE, "Error al eliminar el Administrador", ex.getMessage());
+            return new Respuesta(false,CodigoRespuesta.ERROR_CLIENTE, "Error al eliminar el Administrador", ex.getMessage());
         }
     }
-
     @WebMethod(operationName = "getAdministrador")
     public Respuesta getAdministrador(@WebParam(name = "ID") Long ID) {
         try {
@@ -76,6 +75,51 @@ public class WS {
         }
     }
 
+    /**
+     * Web service operation
+     * @param ID
+     * @return 
+     */
+    @WebMethod(operationName = "getProyecto")
+    public Respuesta getProyecto(@WebParam(name = "ID") Long ID) {
+        try {
+            Respuesta respuesta = proyectoService.getAdministrador(ID);
+            return respuesta;
+        } catch (Exception ex) {
+            Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Error guardando usuario", ex.getMessage());
+        }
+    }
+    
+    /**
+     * Web service operation
+     * @return 
+     */
+    @WebMethod(operationName = "getProyectos")
+    public List<ProyectoDto> getProyectos() {
+        try{
+            return proyectoService.getProyectos();
+        }catch(Exception ex){
+            return null;
+        }
+    }
+    
+    /*@WebMethod(operationName = "getProyectos")
+    public Respuesta getProyectos() {
+        try {
+            Respuesta respuesta = proyectoService.getProyectos();
+            return respuesta;
+        } catch (Exception ex) {
+            Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Error guardando usuario", ex.getMessage());
+        }
+    }
+    */
+    /**
+     * Web service operation
+     * @param proyecto
+     * @return 
+     */
     @WebMethod(operationName = "guardarProyecto")
     public Respuesta guardarProyecto(@WebParam(name = "proyecto") ProyectoDto proyecto) {
         try {
@@ -87,15 +131,127 @@ public class WS {
         }
     }
 
+    /**
+     * Web service operation
+     * @param ID
+     * @return 
+     */
     @WebMethod(operationName = "eliminarProyecto")
     public Respuesta eliminarProyecto(@WebParam(name = "ID") Long ID) {
-        try {
+       try {
             Respuesta respuesta = proyectoService.eliminarProyecto(ID);
             return respuesta;
         } catch (Exception ex) {
             Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
-            return new Respuesta(false, CodigoRespuesta.ERROR_CLIENTE, "Error al eliminar el proyecto.", ex.getMessage());
+            return new Respuesta(false,CodigoRespuesta.ERROR_CLIENTE, "Error al eliminar el proyecto.", ex.getMessage());
         }
     }
-}
 
+    /**
+     * Web service operation
+     * @param adminPorPro
+     * @return 
+     */
+  
+
+    /**
+     * Web service operation
+     * @param actividad
+     * @return 
+     */
+    @WebMethod(operationName = "guardarActividad")
+    public Respuesta guardarActividad(@WebParam(name = "actividad") ActividadesDto actividad) {
+        //TODO write your implementation code here:
+        try {
+            Respuesta respuesta = actividadService.guardarActividades(actividad);
+            return respuesta;
+        } catch (Exception ex) {
+            Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Error guardando la Actividad", ex.getMessage());
+            
+        }
+    }
+
+    /**
+     * Web service operation
+     * @param ID
+     * @return 
+     */
+   /* @WebMethod(operationName = "elminarActividad")
+    public Respuesta elminarActividad(@WebParam(name = "ID") Long ID) {
+        try {
+            Respuesta respuesta = actividadService.eliminarActividad(ID);
+            return respuesta;
+        } catch (Exception ex) {
+            Logger.getLogger(wS.class.getName()).log(Level.SEVERE, null, ex);
+            return new Respuesta(false,CodigoRespuesta.ERROR_CLIENTE, "Error al eliminar la Actividad", ex.getMessage());
+        }
+    }*/
+
+    /**
+     * Web service operation
+     * @param seguimiento
+     * @return 
+     */
+    @WebMethod(operationName = "guardarSeguimiento")
+    public Respuesta guardarSeguimiento(@WebParam(name = "seguimiento") SeguimientoDto seguimiento) {
+        //TODO write your implementation code here:
+        try {
+            Respuesta respuesta = seguimientoService.guardarSeguimiento(seguimiento);
+            return respuesta;
+        } catch (Exception ex) {
+            Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Error guardando el Seguimiento", ex.getMessage());
+        }
+    }
+
+    /**
+     * Web service operation
+     * @param ID
+     * @return 
+     */
+    @WebMethod(operationName = "eliminarSeguimiento")
+    public Respuesta eliminarSeguimiento(@WebParam(name = "ID") Long ID) {
+        try {
+            Respuesta respuesta = seguimientoService.eliminarSeguimiento(ID);
+            return respuesta;
+        } catch (Exception ex) {
+            Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
+            return new Respuesta(false,CodigoRespuesta.ERROR_CLIENTE, "Error al eliminar el seguimiento", ex.getMessage());
+        }
+    }
+
+    /**
+     * Web service operation
+     * @param ID
+     * @return 
+     */
+    @WebMethod(operationName = "eliminarActividad")
+    public Respuesta eliminarActividad(@WebParam(name = "ID") Long ID) {
+        //TODO write your implementation code here:
+        try {
+            Respuesta respuesta = actividadService.eliminarActividades(ID);
+            return respuesta;
+        } catch (Exception ex) {
+            Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
+            return new Respuesta(false,CodigoRespuesta.ERROR_CLIENTE, "Error al eliminar la Actividad", ex.getMessage());
+        }
+    }
+
+    /**
+     * Web service operation
+     * @param Administrador
+     * @return 
+     */
+    @WebMethod(operationName = "guardarAdministrador")
+    public Respuesta guardarAdministrador(@WebParam(name = "Administrador") AdministradorDto Administrador) {
+         try {
+            Respuesta respuesta = administradorService.guardarAdministrador(Administrador);
+            return respuesta;
+        } catch (Exception ex) {
+            Logger.getLogger(WS.class.getName()).log(Level.SEVERE, null, ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Error guardando usuario", ex.getMessage());
+        }
+    }
+    
+}
